175dbde54250f21a675d6ff6cddb4ffbe2475fd0
Guennoune Abderrahim <aguennoune@outlook.com>
2021-05-01 09:17:15 +0000

    Changes to be committed: 	new file:   Download/djangoresume.zip 	modified:   images/profile-1.jpeg 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Qualified Lead Manager", "Freelancer"],
+        strings: ["Developer", "Marketer","Designer", "Event Manager", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Qualified Lead Manager", "Freelancer"],
+        strings: ["Developer", "Marketer", "Designer", "Event Manager", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
