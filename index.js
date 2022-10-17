652e24ccfec34eff2be5c087d697fa2f27027f63
Guennoune Abderrahim <aguennoune@outlook.com>
2022-10-17 07:20:01 +0100

    Changes made

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Features Engineering", "Marketer", "Designer", "Data Engineer"],
+        strings: ["your", "wishes", "are", "our", "commands"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
